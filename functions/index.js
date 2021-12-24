4f93ceba441cd8fbb624997ebb6f12f8e75fac67
Piyush Nanwani <35925360+piyushnanwani@users.noreply.github.com>
2021-12-24 12:15:02 +0530

    Add files via upload

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
+const createUser = require("./create_user");
+const serviceAccount = require("./service_account.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:
+    "https://one-time-password-75a25-default-rtdb.asia-southeast1.firebasedatabase.app",
+});
+
+exports.createUser = functions.https.onRequest(createUser);
